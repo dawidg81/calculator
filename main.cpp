2bd68669a9f51c4e7d89229f4d5c8f2b6448c8a7
dawidg81 <dawidgorski.m@gmail.com>
2026-06-21 16:19:51 +0200

    Implemented events for buttons

--- main.cpp
+++ main.cpp
@@ -63,7 +63,47 @@ void on_operator_button(Fl_Widget*, void* data) {
 		current_input = std::to_string(result);
 
 		current_input.erase(current_input.find_last_not_of('0') + 1, std::string::npos);
+
+		if (current_input.back() == '.') current_input.pop_back();
+		else if (!current_input.empty()) number1 = std::stof(current_input);
+
+		current_operator = op;
+		current_input = "";
+		update_display("0");
 	}
+}
+
+void on_equals(Fl_Widget*, void*) {
+	if (current_operator == '\0' || current_input.empty()) return;
+
+	number2 = std::stof(current_input);
+	float result = calculate(number1, number2, current_operator);
+
+	std::string result_str = std::to_string(result);
+	result_str.erase(result_str.find_last_not_of('0') + 1, std::string::npos);
+	if (result_str.back() == '.') result_str.pop_back();
+
+	number1 = result;
+	current_operator = '\0';
+	current_input = "";
+
+	update_display(result_str);
+}
+
+void on_clear(Fl_Widget*, void*) {
+	number1 = 0;
+	number2 = 0;
+
+	current_operator = '\0';
+	current_input = "";
+
+	update_display(0);
+}
+
+void on_decimal(Fl_Widget*, void*) {
+	if (current_input.empty()) current_input = "0";
+	if (current_input.find('.') == std::string::npos) current_input += '.';
+	update_display(current_input);
 }
 
 // Menu events
@@ -96,6 +136,11 @@ int main(int argc, char** argv) {
 	menu->add("&Help/&About",        0, on_about);
 	menu->add("&Help/&Usage",        0, on_usage);
 
+	main_buffer = new Fl_Text_Buffer();
+	main_display = new Fl_Text_Display(10, 35, 380, 70, 0);
+	main_display->buffer(main_buffer);
+	update_display("");
+
 	/// CALCULATOR BUTTONS
 	Fl_Button *calc_button1 = new Fl_Button(10, 150, 60, 30, "1");
 	Fl_Button *calc_button2 = new Fl_Button(80, 150, 60, 30, "2");
@@ -107,13 +152,32 @@ int main(int argc, char** argv) {
 	Fl_Button *calc_button8 = new Fl_Button(80, 230, 60, 30, "8");
 	Fl_Button *calc_button9 = new Fl_Button(150, 230, 60, 30, "9");
 	Fl_Button *calc_button0 = new Fl_Button(80, 270, 60, 30, "0");
+	calc_button1->callback(on_number_button, (void*)"1");
+	calc_button2->callback(on_number_button, (void*)"2");
+	calc_button3->callback(on_number_button, (void*)"3");
+	calc_button4->callback(on_number_button, (void*)"4");
+	calc_button5->callback(on_number_button, (void*)"5");
+	calc_button6->callback(on_number_button, (void*)"6");
+	calc_button7->callback(on_number_button, (void*)"7");
+	calc_button8->callback(on_number_button, (void*)"8");
+	calc_button9->callback(on_number_button, (void*)"9");
+	calc_button0->callback(on_number_button, (void*)"0");
 
 	Fl_Button *calc_button_clear = new Fl_Button(10, 110, 60, 30, "C");
+	Fl_Button *calc_button_equals = new Fl_Button(220, 270, 60, 30, "=");
+	Fl_Button *calc_button_decimal = new Fl_Button(230, 270, 60, 30, ".");
+	calc_button_clear->callback(on_clear);
+	calc_button_equals->callback(on_equals);
+	calc_button_decimal->callback(on_decimal);
 
 	Fl_Button *calc_button_add = new Fl_Button(220, 110, 60, 30, "+");
 	Fl_Button *calc_button_subtract = new Fl_Button(220, 150, 60, 30, "-");
 	Fl_Button *calc_button_multiplicate = new Fl_Button(220, 190, 60, 30, "*");
 	Fl_Button *calc_button_divide = new Fl_Button(220, 230, 60, 30, "/");
+	calc_button_add->callback(on_operator_button, (void*)&op_add);
+	calc_button_subtract->callback(on_operator_button, (void*)&op_subtract);
+	calc_button_multiplicate->callback(on_operator_button, (void*)&op_multiply);
+	calc_button_divide->callback(on_operator_button, (void*)&op_divide);
 
 	win->end();
 	win->show();
