24821d0c45ba55f1e3e5d8680660bcecd4249e63
dawidg81 <dawidgorski.m@gmail.com>
2026-06-21 10:20:23 +0200

    calculatored

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
 #include <FL/Fl_Menu_Bar.H>
 #include <FL/fl_ask.H>
 
-// Forward declarations
 void on_hello(Fl_Widget*, void*);
 void on_exit(Fl_Widget*, void*);
 void on_about(Fl_Widget*, void*);
@@ -18,9 +17,16 @@ void on_exit(Fl_Widget*, void*) {
 
 void on_about(Fl_Widget*, void*) {
 	fl_message(
+			"= Calculator version 0.1.0 =\n"
+			"by Dawid Gorski 2026.");
+}
+
+void on_usage(Fl_Widget*, void*) {
+	fl_message(
+			"= Navigation in menu bar =\n"
 			"Press Alt + Letter, where Letter is marked letter on\n"
 			"menu bar options. While in one of the menus, the same\n"
-			"keybinding applies.");
+			"keybinding applies. Press arrows to navigate between options.");
 }
 
 int main(int argc, char** argv) {
@@ -31,6 +37,9 @@ int main(int argc, char** argv) {
 	Fl_Menu_Bar* menu = new Fl_Menu_Bar(0, 0, 400, 25);
 	menu->add("&File/&Exit",         0, on_exit);
 	menu->add("&Help/&About",        0, on_about);
+	menu->add("&Help/&Usage",        0, on_usage);
+
+	Fl_Button *test_button = new Fl_Button(60, 60, 60, 30, "Test!");
 
 	win->end();
 	win->show();
